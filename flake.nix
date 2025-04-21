6bdc6e1e082d30fcfc34c3e7db88d313c2541c8b
Dino A. Dai Zovi <3083278+ddz@users.noreply.github.com>
2025-04-20 21:18:55 -0400

    Add nShield Solo PCI HSM kernel module

--- flake.nix
+++ flake.nix
@@ -4,7 +4,14 @@
   # Automatically follow the most recent stable nixpkgs channel
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*.tar.gz";
 
-  outputs = { self, nixpkgs }: {
+  inputs.nshield = {
+    url = "github:ddz/ncipher-nshield-solo";
+
+    # Follow nixpkgs input to build kernel module for its kernel version
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, nshield }: {
     # Build an ISO image by default
     packages.x86_64-linux.default = self.packages.x86_64-linux.iso;
     
@@ -18,6 +25,7 @@
       system = "x86_64-linux";
       modules = [
         "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+        nshield.nixosModules.default
         ./configuration.nix
       ];
     };
